1398a4d7a552d680144db56bf450733bdcc5745e
Alan Bardales <alan.g.bardales@gmail.com>
2025-01-07 11:52:45 -0600

    add error handling middleware and integrate morgan for logging

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
 import { connectDB } from './config/database';
 
 import patientRuter from './routes/patient.route';
+import { errorHandler, notFound } from './middleware/error.middleware';
 
 const app = express();
 const port = Bun.env.PORT || 5000;
 
+app.use(morgan(':method :url :status - :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,6 +19,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/patient', patientRuter);
+
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
